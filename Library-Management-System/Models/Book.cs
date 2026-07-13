391fc9bad287b9706efbaf0232adf10fc20c3924
Khaled Hamada <hh371551@gmail.com>
2026-07-13 16:49:03 +0300

    feat(models): add PremiumMember and refactor member & Book  models

--- Library-Management-System/Models/Book.cs
+++ Library-Management-System/Models/Book.cs
@@ -3,7 +3,12 @@ public class Book : LibraryItem, ISearchable
     public string Author { get; set; } = string.Empty;
     public int Year { get; set; }
     public string Genre { get; set; } = string.Empty;
-    public bool IsAvailable { get; set; } = true;
+    public bool IsAvailable { get; set; } 
+    public Book()
+    {
+        AddedDate = DateTime.Now;
+        IsAvailable = true;
+    }
 
     public override string GetInfo()
     {
--- Library-Management-System/Models/Member.cs
+++ Library-Management-System/Models/Member.cs
@@ -4,11 +4,19 @@ public class Member : ISearchable
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public DateTime JoinDate { get; set; } = DateTime.Now;
-    public Book[] BorrowedBooks { get; set; } = new Book[3];
-
+    public  Book[] BorrowedBooks { get; set; } 
+    
+    public Member()
+    {
+        BorrowedBooks = new Book[3];
+    }
     public bool MatchesQuery(string query)
     {
         return Name.Contains(query, StringComparison.OrdinalIgnoreCase) ||
                Email.Contains(query, StringComparison.OrdinalIgnoreCase);
+    }
+    public virtual string GetInfo()
+    {
+        return $"Member Name: {Name}, Email: {Email}, Join Date: {JoinDate.ToShortDateString()}";
     }
 }
